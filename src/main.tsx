69343fc284a46cddb47d2097fa6b69c25e1f518b
Guanran Wang <guanran928@outlook.com>
2026-03-23 17:16:45 +0800

    refactor: use system color scheme

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <App />
-      <Toaster position="bottom-center" />
-    </ThemeProvider>
+    <App />
+    <Toaster position="bottom-center" />
   </StrictMode>,
 );
