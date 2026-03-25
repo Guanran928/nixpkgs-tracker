9a3cd79e2dcc3f60bef4f060a1e3842ab943fc12
Guanran Wang <guanran928@outlook.com>
2026-03-25 22:28:03 +0800

    chore: respect system motion settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "@/components/ui/sonner";
+import { MotionConfig } from "motion/react";
+
 import "./index.css";
 import App from "./App.tsx";
-import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster position="bottom-center" />
+    <MotionConfig reducedMotion="user">
+      <App />
+      <Toaster position="bottom-center" />
+    </MotionConfig>
   </StrictMode>,
 );
